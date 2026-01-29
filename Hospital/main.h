8efe2fbf129bd62d9c2b1405d3ec0b59bd30cdce
matin <matin.akbari06ap@gmail.com>
2026-01-29 20:31:44 +0330

    test main for check

--- Hospital/main.h
+++ Hospital/main.h
@@ -5,12 +5,5 @@
 #ifndef HOSPITAL_MAIN_H
 #define HOSPITAL_MAIN_H
 
-// Include data storage system
-#include "DataStorage.h"
-
-// Include all hospital management classes
-#include "Person.h"
-#include "Patient.h"
-#include "Staff.h"
 
 #endif //HOSPITAL_MAIN_H
